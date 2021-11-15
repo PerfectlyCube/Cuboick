5805e2d217849d1775c64244f163ea583c69638e
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-11-15 17:51:33 -0500

    Added all the HTML and JS stuff

---
+++ js/game.js
@@ -0,0 +1,37 @@
+const canvas = document.createElement(HTMLCanvasElement);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.setAttribute("width", width);
+canvas.setAttribute("height", height);
+document.body.append(canvas);
+
+const g = canvas.getContext('2d');
+
+function loop() {
+
+    g.fillStyle = "#dfdfdf";
+    g.fillRect(0, 0, width, height);
+    
+    drawCube(1, 1);
+
+}
+
+function drawCube(x, y, color) {
+
+	g.fillStyle = "#" + color;
+  
+	g.beginPath();
+  g.moveTo(25 + x, 25 - 25 + y);
+  g.lineTo(75 + x, 25 - 25 + y);
+  g.lineTo(75 + x, 75 - 25 + y);
+  g.lineTo(50 + x, 100 - 25 + y);
+  g.lineTo(0 + x, 100 - 25 + y);
+  g.lineTo(0 + x, 50 - 25 + y);
+  g.lineTo(25 + x, 25 - 25 + y);
+  g.fill();
+
+}
+
+setInterval(loop, 10);
