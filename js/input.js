ee953da7f7ba090d24d63c3b529453eef7c99656
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-11-16 12:27:57 -0500

    Added buttons for mobile to press.

--- js/input.js
+++ js/input.js
@@ -36,3 +36,28 @@ addEventListener("keyup", (e) => { keyinput(false, e) });
 
 addEventListener("mousedown",(e) => { mouseinput(true, e) });
 addEventListener("mouseup", (e) => { mouseinput(false, e) });
+
+document.getElementById("left").addEventListener("click", (e) => {
+
+    left = true;
+    left = false;
+
+})
+document.getElementById("up").addEventListener("click", (e) => {
+
+    up = true;
+    up = false;
+
+})
+document.getElementById("down").addEventListener("click", (e) => {
+
+    down = true;
+    down = false;
+
+})
+document.getElementById("right").addEventListener("click", (e) => {
+
+    right = true;
+    right = false;
+
+})
