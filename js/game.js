bf3bf1b3e5b4abfcc35a7ce8a7197fa6c06ca8b5
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-11-16 12:09:10 -0500

    Change in screen size

--- js/game.js
+++ js/game.js
@@ -9,8 +9,8 @@ let x = 0, y = 0, z = 0;
 
 const canvas = document.createElement("canvas");
 
-const width = window.innerWidth - 800/12;
-const height = window.innerHeight - 600/12;
+const width = window.innerWidth - 50 * 12;
+const height = window.innerHeight - 50 * 8;
 
 canvas.setAttribute("id", "screen");
 canvas.setAttribute("width", width);
